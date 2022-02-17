bcaec5dfd0d07fe283fc7634a796787e3a2ed752
Kishan Kharwar <kishan.kharwar@kroo.com>
2022-02-17 21:53:42 +0530

    Deny unencrypted inflight operation for access-log-bucket
    Authors : @claudiodavid-ai @KishanAnjanaKharwar

--- main.tf
+++ main.tf
@@ -25,10 +25,29 @@ data "template_file" "encrypted_bucket_policy" {
   }
 }
 
-resource "aws_s3_bucket" "access_log_bucket" {
-  bucket = (var.access_log_bucket_name != null
+locals {
+  access_bucket_name = (var.access_log_bucket_name != null
   ? var.access_log_bucket_name
   : "${var.bucket_name}-access-log")
+}
+data "template_file" "deny_unencrypted_inflight_operations_fragment_access_bucket" {
+  template = file("${path.module}/policy-fragments/deny-unencrypted-inflight-operations.json.tpl")
+  vars = {
+    bucket_name = local.access_bucket_name
+  }
+}
+
+data "template_file" "access_bucket_policy" {
+  template = file("${path.module}/policies/access-bucket-policy.json.tpl")
+
+  vars = {
+    bucket_name = local.access_bucket_name
+    deny_unencrypted_inflight_operations_fragment = data.template_file.deny_unencrypted_inflight_operations_fragment_access_bucket.rendered
+  }
+}
+
+resource "aws_s3_bucket" "access_log_bucket" {
+  bucket = local.access_bucket_name
   acl    = "log-delivery-write"
   count = var.enable_access_logging == "yes" ? 1 : 0
 
@@ -101,3 +120,18 @@ resource "aws_s3_bucket_policy" "encrypted_bucket" {
   bucket = aws_s3_bucket.encrypted_bucket.id
   policy = data.aws_iam_policy_document.encrypted_bucket_policy_document.json
 }
+
+data "aws_iam_policy_document" "access_bucket_policy_document" {
+  source_json = var.source_policy_json
+  override_json = data.template_file.access_bucket_policy.rendered
+}
+
+resource "aws_s3_bucket_policy" "access_bucket" {
+  count = var.enable_access_logging == "yes" ? 1 : 0
+  bucket = aws_s3_bucket.access_log_bucket[0].id
+  policy = data.aws_iam_policy_document.access_bucket_policy_document.json
+}
+
+
+
+
