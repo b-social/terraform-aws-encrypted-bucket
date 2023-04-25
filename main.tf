db1541f9001558c4e434de69c52467c3244708ff
Ben Cross <ben.cross@kroo.com>
2023-04-25 16:52:30 +0100

    [TECH] Change object ownership to object writer

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_s3_bucket_ownership_controls" "encrypted_bucket_ownership_controls
   bucket = aws_s3_bucket.encrypted_bucket.id
 
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "ObjectWriter"
   }
 }
 
